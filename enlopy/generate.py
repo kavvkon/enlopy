Small bugfix on plot_LDC axis scaling. Improved testing and code quality.

enlopy/generate.py
@@ -22,7 +22,7 @@ _EPS = np.finfo(np.float64).eps
 def disag_upsample(Load, disag_profile, to_offset='h'):
     """ Upsample given timeseries, disaggregating based on given load profiles.
     e.g. From daily to hourly. The load of each day is distributed according to the disaggregation profile. The sum of each day remains the same.
-    
+
     Arguments:
         Load (pd.Series): Load profile to disaggregate
         disag_profile (pd.Series, np.ndarray): disaggregation profile to be used on each timestep of the load. Has to be compatible with selected offset.
@@ -339,7 +339,7 @@ def gen_demand_response(Load, percent_peak_hrs_month=0.03, percent_shifted=0.05,
     otherwise it is smaller due to the shaved peaks. The peak load is reduced by a predefined percentage.
      
     Arguments:
-        Load (pd.Series): Load 
+        Load (pd.Series): Load
         percent_peak_hrs_month (float): fraction of hours to be shifted
         percent_shifted (float): fraction of energy to be shifted if the day is tagged for shifting/shaving
         shave (bool): If False peak load will be transfered to low load hours, otherwise it will be shaved.
@@ -355,8 +355,6 @@ def gen_demand_response(Load, percent_peak_hrs_month=0.03, percent_shifted=0.05,
         """Assign to each row hours per month"""
         dic_hours_per_month = demand.groupby(demand.index.month).count().to_dict()
         return demand.resample('m').transform(lambda x: list(map(dic_hours_per_month.get, x.index.month)))
-
-    total_demand = demand.sum()
 
     # Monthly demand rank
     # TODO: parametrize: we can check peaks on a weekly or daily basis

enlopy/plot.py
@@ -13,7 +13,7 @@ __all__ = ['plot_heatmap', 'plot_3d', 'plot_percentiles', 'plot_rug', 'plot_boxp
 def plot_heatmap(Load, x='dayofyear', y='hour', aggfunc='sum', bins=8,
                 palette='Oranges', colorbar=True, ax=None, **pltargs):
     """ Returns a 2D heatmap of the reshaped timeseries based on x, y
-    
+
     Arguments:
         Load: 1D pandas with timed index
         x: Parameter for :meth:`enlopy.analysis.reshape_timeseries`
@@ -139,7 +139,7 @@ def plot_boxplot(Load, by='day', **pltargs):
     # TODO Is it really needed? pd.boxplot()
 
 
-def plot_LDC(Load, x_norm=True, y_norm=False, cmap='Set3', color='black', **kwargs):
+def plot_LDC(Load, x_norm=True, y_norm=False, cmap='Spectral', color='black', **kwargs):
     """Plot Load duration curve
     
     Arguments:
@@ -158,8 +158,10 @@ def plot_LDC(Load, x_norm=True, y_norm=False, cmap='Set3', color='black', **kwar
         # Reconverting to Dataframe as pd.plot.area is much more robust than plt.stackplot
         ldc_frame = pd.DataFrame(y, index=x)
         ldc_frame.plot.area(cmap=cmap, lw=0, legend=False, **kwargs)
+        y_max = np.nanmax(np.nansum(y, axis=1))
     else:
         plt.plot(x, y, color=color)
+        y_max = np.nanmax(y)
     if x_norm:
         plt.xlim(0, 1)
         plt.xlabel('Normalized duration')
@@ -170,7 +172,7 @@ def plot_LDC(Load, x_norm=True, y_norm=False, cmap='Set3', color='black', **kwar
         plt.ylim(0,1)
         plt.ylabel('Normalized Power')
     else:
-        plt.ylim(0, np.nanmax(y))
+        plt.ylim(0, y_max)
         plt.ylabel('Power')
 
 

tests/test_analysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from enlopy.utils import make_timeseries
-from enlopy.analysis import reshape_timeseries, get_LDC
+from enlopy.analysis import reshape_timeseries, get_LDC, get_load_stats
 
 
 def test_reshape_timeseries():
@@ -32,3 +32,12 @@ def test_get_LDC_2d():
     assert np.isclose(np.nansum(b[1]), np.nansum(a))
     # check monotonicity
     assert np.all(np.diff(b[1].sum(1)) < 0)
+
+def test_get_stats():
+    a = np.ones(8760)
+    a = make_timeseries(a, freq='h')
+    b = get_load_stats(a)
+    assert a.sum() == b.loc['Sum'].squeeze()
+    assert np.asarray(b.loc['Ramps (98%)'])[0] == (0,0)
+    assert np.isclose(0, b.loc['Trend'].squeeze())
+    assert 1 == b.loc['Load Factor (peakiness)'].squeeze()

tests/test_generate.py
@@ -129,11 +129,10 @@ def test_gen_corr_arrays():
     Na = 2
     length = 1000
     r = 0.85
-    tol = 0.01
     M = np.array([[1, r],
                   [r, 1]])
     A = gen_corr_arrays(Na, length, M)
     new_r = np.corrcoef(A)[0][1]
     assert A.shape == (Na, length)
     #allow some tolerance of convergence..
-    assert np.abs(new_r - r) <= 0.02
+    assert np.abs(new_r - r) <= 0.03

tests/test_utils.py
@@ -51,7 +51,6 @@ class Test_make_timeseries():
         assert type(b) == pd.Series and len(b)==0
 
     def test_empty_frame_to_indexed_empty(self):
-        a = np.array([])
         b = make_timeseries(freq='H', length=8760)
         assert isinstance(b, pd.Series) and len(b) == 8760
 
@@ -67,27 +66,27 @@ class Test_clean_convert():
     def test_ndarray_to_series_indexed(self):
         a = np.random.rand(8760)
         b = clean_convert(a, force_timed_index=True, always_df=False)
-        assert type(b) == pd.Series and b.index.is_all_dates
+        assert isinstance(b, pd.Series) and b.index.is_all_dates
 
     def test_ndarray_to_df_indexed(self):
         a = np.random.rand(8760)
         b = clean_convert(a, force_timed_index=True, always_df=True)
-        assert type(b) == pd.DataFrame and b.index.is_all_dates
+        assert isinstance(b, pd.DataFrame) and b.index.is_all_dates
 
     def test_1d_series_to_frame(self):
         a = pd.Series(np.random.rand(8760))
         b = clean_convert(a, force_timed_index=True, always_df=True)
-        assert type(b) == pd.DataFrame and b.index.is_all_dates
+        assert isinstance(b, pd.DataFrame) and b.index.is_all_dates
 
     def test_2d_ndarray_to_df_indexed(self):
         a = np.random.rand(8760, 2)
         b = clean_convert(a, force_timed_index=True, always_df=True)
-        assert type(b) == pd.DataFrame and b.index.is_all_dates
+        assert isinstance(b, pd.DataFrame) and b.index.is_all_dates
 
     def test_list_to_series(self):
         a = list(np.random.rand(8760))
         b = clean_convert(a, force_timed_index=True, always_df=False)
-        assert type(b) == pd.Series
+        assert isinstance(b, pd.Series)
 
 
 def test_freq_to_sec():